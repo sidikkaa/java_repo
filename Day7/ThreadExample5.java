06a5acb3237163cd5dfcbd59741df705fcf89b40
sidikkaa <151637077+sidikkaa@users.noreply.github.com>
2025-02-13 10:21:00 +0530

    Update ThreadExample5.java

--- Day7/ThreadExample5.java
+++ Day7/ThreadExample5.java
@@ -1,9 +1,66 @@
 package com.celcom.day7;
 
+class Resource {
+	private String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+}
+class Reader extends Thread{
+	Resource resource ;
+	
+	Reader(Resource resource){
+		this.resource=resource;
+	}
+	
+	public void run() {
+		synchronized(resource) {
+			System.out.println("Reader is waiting for writer to write the data");
+			try {
+				resource.wait();
+			} catch (InterruptedException e) {
+				
+			}
+			System.out.println("Data from Reader:"+resource.getData());
+		}
+		
+	}
+}
+class Writer extends Thread{
+	Resource resource ;
+	
+	Writer(Resource resource){
+		this.resource=resource;
+	}
+	
+	public void run() {
+		synchronized(resource) {
+			System.out.println(" writer is writing the data...");
+			resource.setData("Hello Reader");
+			resource.notify();
+			
+		}
+		
+	}
+}
 public class ThreadExample5 {
 
-	public static void main(String args[] {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		Resource resource=new Resource();
+		
+		Reader reader=new Reader(resource);
+		reader.start();
+		
+		Thread.sleep(2000);
+		
+		Writer writer=new Writer(resource);
+		writer.start();
 
 	}
 
